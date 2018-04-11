54d82fcee754a6134fda62d50e94b820de3a262b
Charlotte <burmeisc@tcd.ie>
2018-04-11 09:34:50 +0200

    new file structure

--- distributed_computing/client.test.py
+++
@@ -1,8 +0,0 @@
-import xmlrpclib
-
-
-server = xmlrpclib.ServerProxy('http://localhost:8001')
-
-#server.register_introspection_functions()
-print server.get_angle('HeadYaw')
-print server.system.listMethods()
