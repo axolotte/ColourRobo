d0e2ba9780fe014d06ce35a78bd63b3072d72b0a
Charlotte <burmeisc@tcd.ie>
2018-04-26 15:58:52 +0200

    Main Listen Loop works, meaning robo listen to color and then detects color.

--- vision/color_recognition.py
+++ vision/color_recognition.py
@@ -14,7 +14,7 @@ NAO_IP = "nao2.local"
 
 # Global variable to store the colorBlob module instance
 colorBlob = None
-memory = None
+#memory = None
 
 
 class ColorDetectionModule(ALModule):
@@ -29,13 +29,15 @@ class ColorDetectionModule(ALModule):
         # Subscribe to the ColorRecognition event:
         global memory
         memory = ALProxy("ALMemory")
+        self.subscribeToBlopDetection("red")
 
 
     def subscribeToBlopDetection(self,color):
         """subscribe to blop event"""
-        self._blobProxy = ALProxy("ALColorBlobDetection")
-        self._blobProxy.setColor(255, 0, 0, 50)
-        self._blobProxy.setObjectProperties(10, 5, "Circle")
+        self.blobProxy = ALProxy("ALColorBlobDetection")
+        #TO DO: choose color
+        self.blobProxy.setColor(255, 0, 0, 50)
+        self.blobProxy.setObjectProperties(10, 5, "Circle")
 
 
         memory.subscribeToEvent("ALTracker/ColorBlobDetected",
@@ -46,17 +48,21 @@ class ColorDetectionModule(ALModule):
 
 
     def onColorDetected(self, *_args):
-        """ This will be called each time a face is
+        """ This will be called each time a color is
         detected.
 
         """
         # Unsubscribe to the event when talking,
         # to avoid repetitions
-        self._getCircle = self._blobProxy.getCircle()
-        print self._getCircle
+        getCircle = self.blobProxy.getCircle()
+        print "colorseen"
+        print getCircle
 
         memory.unsubscribeToEvent("ALTracker/ColorBlobDetected",
             "colorBlob")
+
+        #Do stuff here
+        time.sleep(3)
 
         # Subscribe again to the event
         memory.subscribeToEvent("ALTracker/ColorBlobDetected",
