189084d66316b629a5a29dadc3d6ad483aa34dba
Freudendthaler Franz <freudendthaler.franz@gmail.com>
2018-05-22 12:53:05 +0200

    final folderstructure

--- audio/hello_world.py
+++
@@ -1,3 +0,0 @@
-from naoqi import ALProxy
-tts = ALProxy("ALTextToSpeech", "10.0.7.13", 9559)
-tts.say("So long, and thank's for the fish!")
--- hello_world.py
+++
@@ -1,3 +0,0 @@
-from naoqi import ALProxy
-tts = ALProxy("ALTextToSpeech","127.0.0.1",9559)
-tts.say("HelloWorld")
